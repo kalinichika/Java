a194081e1a711cb198f04a8a0d51551e8094784c
kalinichika <viktoriya.v.kalinina@yandex.ru>
2019-10-14 18:43:45 +0300

    First Java project

---
+++ Java tests/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.util.*;
+import java.util.ArrayList;
+
+class CommonElementSearcher {
+    private List<Integer> _l1;
+    private List<Integer> _l2;
+    private Integer element = 0;
+
+
+    CommonElementSearcher(List<Integer> l1, List<Integer> l2) {
+        _l1 = new ArrayList<Integer>(l1);
+        _l2 = new ArrayList<Integer>(l2);
+    }
+}
+public class MyClass implements Cloneable{
+
+}
+
+public class Main {
+
+}
+
+public static void main(String[] args){
+    List<Integer> l1 = new ArrayList<Integer>();
+    List<Integer> l2 = new ArrayList<Integer>();
+
+    CommonE
+}
